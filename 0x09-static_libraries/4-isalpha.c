9cb9db154a706f6057e00a7332a6a5dfdf04feca
Michael Ahere <mikeahere@gmail.com>
2023-10-27 12:33:58 -0700

    Waah

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,13 @@
 #include "main.h"
-		
+
 /**
- * _isalpha - checks for alphabetic character
+ * is_alpha - checks if a character is an alphabetic character
  * @c: the character to be checked
+ *
  * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int is_alpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+

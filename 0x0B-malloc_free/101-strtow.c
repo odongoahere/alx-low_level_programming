e602a62e8bfa57db7701c9de402f9d57819d4644
Michael Ahere <mikeahere@gmail.com>
2023-10-30 23:49:10 -0700

    Another Finale

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,60 +5,60 @@ void util(char **, char *);
 void create_word(char **, char *, int, int, int);
 
 /**
-* strtow - splits a string into words.
-* @str: the string
-*
-* Return: returns a pointer to an array of strings (words)
-*/
+ * strtow - splits a string into words.
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
+ */
 char **strtow(char *str)
 {
-  int i, flag, len;
-  char **words;
+	int i, flag, len;
+	char **words;
 
-  if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-     return (NULL);
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+		return (NULL);
 
-  i = flag = len = 0;
-  while (str[i])
-  {
-     if (flag == 0 && str[i] != ' ')
-        flag = 1;
-     if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-     {
-        flag = 0;
-        len++;
-     }
-     i++;
-  }
+	i = flag = len = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+		i++;
+	}
 
-  len += flag == 1 ? 1 : 0;
-  if (len == 0)
-     return (NULL);
+	len += flag == 1 ? 1 : 0;
+	if (len == 0)
+		return (NULL);
 
-  words = (char **)malloc(sizeof(char *) * (len + 1));
-  if (words == NULL)
-     return (NULL);
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+		return (NULL);
 
-  util(words, str);
-  words[len] = NULL;
-  return (words);
+	util(words, str);
+	words[len] = NULL;
+	return (words);
 }
 
 /**
-* util - a util function for fetching words into an array
-* @words: the strings array
-* @str: the string
-*/
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
 void util(char **words, char *str)
 {
-  int i, j, start, flag;
+	int i, j, start, flag;
 
-  i = j = flag = 0;
-  while (str[i])
-  {
-     if (flag == 0 && str[i] != ' ')
-     {
-        start = i;
-        flag = 1;
-     }
+	i = j = flag = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
 
